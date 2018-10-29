c8624dca512addd1c4742b8937a3b1025f603dfe
Oliver Austin <15oa16@caslab.queensu.ca>
2018-10-29 16:13:08 -0400

    TCO-51 Activity Switching

---
+++ app/src/main/java/com/example/poe/tutorstage1/LandingActivity.java
@@ -0,0 +1,40 @@
+package com.example.poe.tutorstage1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LandingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+        Button mStudentActivity = (Button)findViewById(R.id.studentActivityButton);
+        mStudentActivity.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                switchStudentActivity(view);
+            }
+        });
+        Button mTutorActivity = (Button)findViewById(R.id.tutorActivityButton);
+        mTutorActivity.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                switchTutorActivity(view);
+            }
+        });
+
+        // Example of a call to a native method
+//        TextView tv = (TextView) findViewById(R.id.sample_text);
+//        tv.setText(stringFromJNI());
+    }
+    public void switchStudentActivity(View view) {
+        Intent intent = new Intent(this, StudentProfileActivity.class);
+        startActivity(intent);
+    }
+    public void switchTutorActivity(View view) {
+        Intent intent = new Intent(this, TutorProfileActivity.class);
+        startActivity(intent);
+    }
+}
--- app/src/main/java/com/example/poe/tutorstage1/MainActivity.java
+++ app/src/main/java/com/example/poe/tutorstage1/MainActivity.java
@@ -1,29 +1,43 @@
 package com.example.poe.tutorstage1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
+    //static {
+       // System.loadLibrary("native-lib");
+//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button mLandingActivity = (Button)findViewById(R.id.landingActivityButton);
+        mLandingActivity.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                switchLandingActivity(view);
+            }
+        });
 
         // Example of a call to a native method
 //        TextView tv = (TextView) findViewById(R.id.sample_text);
 //        tv.setText(stringFromJNI());
+    }
+    public void switchLandingActivity(View view) {
+        Intent intent = new Intent(this, LandingActivity.class);
+        startActivity(intent);
     }
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI();
+    //public native String stringFromJNI();
 }
