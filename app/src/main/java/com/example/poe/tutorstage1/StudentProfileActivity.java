ef3b449794cb8fe04fb21f1d65ae0c6368cd4029
Oliver Austin <15oa16@caslab.queensu.ca>
2018-11-06 13:45:17 -0500

    Student profile fields toggle focusable on edit button click

--- app/src/main/java/com/example/poe/tutorstage1/StudentProfileActivity.java
+++ app/src/main/java/com/example/poe/tutorstage1/StudentProfileActivity.java
@@ -3,16 +3,23 @@ package com.example.poe.tutorstage1;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class StudentProfileActivity extends AppCompatActivity {
-
+private EditText mName;
+private EditText mClass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_profile);
+        mName = findViewById(R.id.userName);
+        mClass = findViewById(R.id.classList);
+        mName.setFocusable(true);
+        mClass.setFocusable(true);
         Button mTutorListActivity = (Button)findViewById(R.id.tutorListActivityButton);
         mTutorListActivity.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view){
@@ -20,11 +27,9 @@ public class StudentProfileActivity extends AppCompatActivity {
             }
         });
 
-        Button mStudentProfileEditActivity = (Button)findViewById(R.id.enterEditMode);
-        mStudentProfileEditActivity.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View view){
-                switchStudentProfileEditActivity(view);
-            }
+        Button mStudentProfileEdit = (Button)findViewById(R.id.enterEditMode);
+        mStudentProfileEdit.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){toggleEditProfile();}
         });
 
     }
@@ -32,11 +37,14 @@ public class StudentProfileActivity extends AppCompatActivity {
         Intent intent = new Intent(this, TutorListActivity.class);
         startActivity(intent);
     }
-
-    public void switchStudentProfileEditActivity(View view) {
-        Intent intent = new Intent(this, StudentProfileEditActivity.class);
-        startActivity(intent);
+    public void toggleEditProfile(){
+        if(mName.isFocusable()){
+            mName.setFocusable(false);
+            mClass.setFocusable(false);
+        }
+        else{
+            mName.setFocusable(true);
+            mClass.setFocusable(true);
+        }
     }
-
-
 }
