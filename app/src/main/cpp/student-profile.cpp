11c3806b4bd9536417ca6d7a0f4f25a409969fa5
Oliver Austin <15oa16@caslab.queensu.ca>
2018-11-06 14:30:14 -0500

    passing variables through c++ works

--- app/src/main/cpp/student-profile.cpp
+++ app/src/main/cpp/student-profile.cpp
@@ -3,11 +3,11 @@
 //
 
 #include <string>
-#include "StudentProfile.h"
+#include "student-profile.h"
 
 extern "C" {
 JNIEXPORT jstring JNICALL
-Java_com_example_poe_tutorstage1_StudentProfileEditActivity_saveProfile(JNIEnv *env, jobject ,
+Java_com_example_poe_tutorstage1_StudentProfileActivity_saveProfile(JNIEnv *env, jobject ,
                                                                         jstring userName) {
     const char *path = env->GetStringUTFChars(userName, NULL);
     std::string userNameJava = std::string(path);
