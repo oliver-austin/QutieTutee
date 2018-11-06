f1946776650ee5c4ea18319f9d2dd96951ca9409
Oliver Austin <15oa16@caslab.queensu.ca>
2018-11-06 17:03:57 -0500

    Communication with C++ layer and edit visibility work

--- app/src/main/cpp/student-profile.cpp
+++ app/src/main/cpp/student-profile.cpp
@@ -1,14 +1,10 @@
-//
-// Created by Poe on 11/5/2018.
-//
-
+#include <jni.h>
 #include <string>
-#include "student-profile.h"
 
 extern "C" {
 JNIEXPORT jstring JNICALL
 Java_com_example_poe_tutorstage1_StudentProfileActivity_saveProfile(JNIEnv *env, jobject ,
-                                                                        jstring userName) {
+                                                                    jstring userName) {
     const char *path = env->GetStringUTFChars(userName, NULL);
     std::string userNameJava = std::string(path);
     return env->NewStringUTF(userNameJava.c_str());
