c30ff7d15ae472541ca3776a5a38ae5638087103
heyselt <15tjh2@queensu.ca>
2018-11-07 13:24:07 -0500

    TCO-24/25 Search page with list items in place. Starting to work on expandability of list items and json calls

---
+++ app/src/main/java/com/example/poe/tutorstage1/tutorListItem.java
@@ -0,0 +1,19 @@
+package com.example.poe.tutorstage1;
+
+public class tutorListItem {
+    private String head;
+    private String desc;
+
+    public tutorListItem(String head, String desc) {
+        this.head = head;
+        this.desc = desc;
+    }
+
+    public String getHead() {
+        return head;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+}
