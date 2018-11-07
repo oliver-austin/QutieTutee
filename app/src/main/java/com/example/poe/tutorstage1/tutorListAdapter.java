c067d38207216bf79d8cb2029236ed9069f8139c
heyselt <15tjh2@queensu.ca>
2018-11-07 13:24:07 -0500

    TCO-24/25 Search page with list items in place. Starting to work on expandability of list items and json calls

---
+++ app/src/main/java/com/example/poe/tutorstage1/tutorListAdapter.java
@@ -0,0 +1,35 @@
+package com.example.poe.tutorstage1;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class tutorListAdapter extends RecyclerView.Adapter<tutorListAdapter.ViewHolder> {
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.support_tutor_search, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
+}
