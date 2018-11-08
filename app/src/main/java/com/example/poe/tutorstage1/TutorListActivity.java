6bf5abda971171832425c7f8dc4adb0e2d8c79d3
heyselt <15tjh2@queensu.ca>
2018-11-08 16:01:39 -0500

    TCO-24/25 completed tutor search page with filler info. Ready to be linked to database

--- app/src/main/java/com/example/poe/tutorstage1/TutorListActivity.java
+++ app/src/main/java/com/example/poe/tutorstage1/TutorListActivity.java
@@ -4,10 +4,19 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TutorListActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private List<tutorListItem> listItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +24,19 @@ public class TutorListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_tutor_search);
         //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         //setSupportActionBar(toolbar);
+
+        recyclerView = (RecyclerView) findViewById(R.id.tutorList);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        listItems = new ArrayList<>();
+
+        for(int i= 0; i<10; i++){
+            tutorListItem aTutor = new tutorListItem("heading" + (i+1), "Filler info");
+            listItems.add(aTutor);
+        }
+
+        adapter = new tutorListAdapter(listItems, this);
+        recyclerView.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/example/poe/tutorstage1/tutorListAdapter.java
+++ app/src/main/java/com/example/poe/tutorstage1/tutorListAdapter.java
@@ -1,12 +1,26 @@
 package com.example.poe.tutorstage1;
 
+import android.app.LauncherActivity;
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import java.util.List;
+
+import org.w3c.dom.Text;
 
 public class tutorListAdapter extends RecyclerView.Adapter<tutorListAdapter.ViewHolder> {
+    private List<tutorListItem> listItems;
+    private Context context;
+
+    public tutorListAdapter(List<tutorListItem> listItems, Context context) {
+        this.listItems = listItems;
+        this.context = context;
+    }
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -17,18 +31,29 @@ public class tutorListAdapter extends RecyclerView.Adapter<tutorListAdapter.View
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        tutorListItem listItem = listItems.get(position);
 
+        holder.listItemHead.setText(listItem.getHead());
+        holder.tutorDescription.setText(listItem.getDesc());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return listItems.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
+        public TextView listItemHead;
+        public TextView tutorDescription;
+
+
+
         public ViewHolder(View itemView) {
             super(itemView);
+
+            listItemHead = (TextView) itemView.findViewById(R.id.listItemHead);
+            tutorDescription= (TextView) itemView.findViewById(R.id.tutorDescription);
         }
     }
 
