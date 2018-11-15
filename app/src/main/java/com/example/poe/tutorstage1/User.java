8e7e4e39140872cc7b9d0ec9d1410d9ed60f696d
Oliver Austin <15oa16@caslab.queensu.ca>
2018-11-14 19:00:22 -0500

    Integrated retrofit for API calls

--- app/src/main/java/com/example/poe/tutorstage1/User.java
+++ app/src/main/java/com/example/poe/tutorstage1/User.java
@@ -5,10 +5,49 @@ package com.example.poe.tutorstage1;
 public class User implements Serializable {
     public User(){
         message = "test";
+        name = null;
+        s_courses = null;
+        t_courses = null;
+        email = null;
     }
     private String message;
+    String name;
+    String s_courses;
+    String t_courses;
+    String email;
 
     public String getMessage() {
         return message;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getS_courses() {
+        return s_courses;
+    }
+
+    public void setS_courses(String s_courses) {
+        this.s_courses = s_courses;
+    }
+
+    public String getT_courses() {
+        return t_courses;
+    }
+
+    public void setT_courses(String t_courses) {
+        this.t_courses = t_courses;
     }
 }
