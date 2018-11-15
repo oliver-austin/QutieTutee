62c2688d6302a21465273888bde475a133e7f24a
David Robinson <14dr21@queensu.ca>
2018-11-14 22:43:49 -0500

    untested code

--- app/src/main/java/com/example/poe/tutorstage1/API.java
+++ app/src/main/java/com/example/poe/tutorstage1/API.java
@@ -3,8 +3,11 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Body;
 import retrofit2.http.Path;
-import retrofit2.http.Query;
+
 
 public interface API {
 
@@ -13,4 +16,14 @@ public interface API {
 
     @GET("user/tutors")
     Call<List<User>> getTutors();
+
+    @GET("user/{email}/{tutor}")
+    Call<User> getUser(@Path("email") String email, @Path("tutor") boolean tutor);
+
+    @POST("user")
+    Call<User> newUser(@Body User newUser);
+
+    @PUT("user/{email}/{tutor}")
+    Call<User> updateUser(@Path("email") String email, @Path("tutor") boolean tutor, @Body User user);
+
 }
--- app/src/main/java/com/example/poe/tutorstage1/APIController.java
+++ app/src/main/java/com/example/poe/tutorstage1/APIController.java
@@ -15,7 +15,7 @@ public class APIController {
 
     static final String BASE_URL = "http://collingwood.caslab.queensu.ca:5010/";
 
-    public void start(String apiEndpoint) {
+    public void start(int select, User user) {
         Gson gson = new GsonBuilder()
                 .setLenient()
                 .create();
@@ -28,45 +28,111 @@ public class APIController {
                 .build();
 
         API api = retrofit.create(API.class);
-        if(apiEndpoint.equals("tutors")){
-            Call<List<User>> call = api.getTutors();
-            call.enqueue(new Callback<List<User>>() {
-                @Override
-                public void onResponse(Call<List<User>> call, Response<List<User>> response) {
-                    if (response.isSuccessful()) {
-                        List<User> userList = response.body();
-                        userList.forEach(user -> System.out.println(user.email));
-                    } else {
-                        System.out.println(response.errorBody());
-                    }
-                }
+        switch(select){
 
-                @Override
-                public void onFailure(Call<List<User>> call, Throwable t) {
+            case 0:                 //Get all users
+                Call<List<User>> call = api.getUsers();
+                call.enqueue(new Callback<List<User>>() {
+                    @Override
+                    public void onResponse(Call<List<User>> call, Response<List<User>> response) {
+                        if (response.isSuccessful()) {
+                            List<User> userList = response.body();
+                            userList.forEach(user -> System.out.println(user.email));
+                        } else {
+                            System.out.println(response.errorBody());
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<List<User>> call, Throwable t) {
+                        t.printStackTrace();
+                    }
+
+                });
+
+            case 1:                 //Get all tutors
+                Call<List<User>> call2 = api.getTutors();
+                call2.enqueue(new Callback<List<User>>() {
+                    @Override
+                    public void onResponse(Call<List<User>> call, Response<List<User>> response) {
+                        if (response.isSuccessful()) {
+                            List<User> userList = response.body();
+                            userList.forEach(user -> System.out.println(user.email));
+                        } else {
+                            System.out.println(response.errorBody());
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<List<User>> call, Throwable t) {
                     t.printStackTrace();
                 }
 
-            });
+                });
 
-        } else {
-            Call<List<User>> call = api.getUsers();
-            call.enqueue(new Callback<List<User>>() {
-                @Override
-                public void onResponse(Call<List<User>> call, Response<List<User>> response) {
-                    if (response.isSuccessful()) {
-                        List<User> userList = response.body();
-                        userList.forEach(user -> System.out.println(user.email));
-                    } else {
-                        System.out.println(response.errorBody());
+
+
+            case 2:                 //Get specific user
+                Call<User> call3 = api.getUser(user.getEmail(), user.getUserType());
+                call3.enqueue(new Callback<User>() {
+                    @Override
+                    public void onResponse(Call<User> call, Response<User> response) {
+                        if (response.isSuccessful()) {
+                            User user = response.body();
+                            System.out.println(user.email);
+                        } else {
+                            System.out.println(response.errorBody());
+                        }
                     }
-                }
 
-                @Override
-                public void onFailure(Call<List<User>> call, Throwable t) {
-                    t.printStackTrace();
-                }
+                    @Override
+                    public void onFailure(Call<User> call, Throwable t) {
+                        t.printStackTrace();
+                    }
 
-            });
+                });
+
+
+
+            case 3:
+                Call<User> call4 = api.newUser(user);
+                call4.enqueue(new Callback<User>() {
+                    @Override
+                    public void onResponse(Call<User> call, Response<User> response) {
+                        if (response.isSuccessful()) {
+                            int code = response.code();
+                            System.out.println(code);
+                        } else {
+                            System.out.println(response.errorBody());
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<User> call, Throwable t) {
+                        t.printStackTrace();
+                    }
+
+                });
+
+            case 4:                 //Update specific user
+                Call<User> call5 = api.updateUser(user.getEmail(), user.getUserType(), user);
+                call5.enqueue(new Callback<User>() {
+                    @Override
+                    public void onResponse(Call<User> call, Response<User> response) {
+                        if (response.isSuccessful()) {
+                            User updatedUser = response.body();
+                            System.out.println(updatedUser.email);
+                        } else {
+                            System.out.println(response.errorBody());
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<User> call, Throwable t) {
+                        t.printStackTrace();
+                    }
+
+                });
         }
 
     }
