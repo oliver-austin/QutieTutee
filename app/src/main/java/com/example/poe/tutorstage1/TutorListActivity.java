4162c7eef3968c25453d05421a64214fd3c88a29
heyselt <15tjh2@queensu.ca>
2018-11-19 22:05:13 -0500

    TCO-86 committing before I change too much

--- app/src/main/java/com/example/poe/tutorstage1/TutorListActivity.java
+++ app/src/main/java/com/example/poe/tutorstage1/TutorListActivity.java
@@ -1,6 +1,7 @@
 package com.example.poe.tutorstage1;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -19,18 +20,60 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TutorListActivity extends AppCompatActivity {
-
     private RecyclerView recyclerView;
     private tutorListAdapter adapter;
     private List<tutorListItem> listItems;
+    private List<User> allUsers;
+    private List<User> releventUsers;
+    private User foo = new User();
     private PopupWindow popupWindow;
     private LayoutInflater layoutInflater;
 
+    static {
+        System.loadLibrary("sign-in");
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
+        APIController controller = new APIController();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutor_search);
         long ptr = (long)getIntent().getSerializableExtra("userPointer");
+
+        controller.start(0, foo, new APICallbacks() {
+            @Override
+            public void onSuccess(@NonNull User user) {
+                allUsers.add(user);
+            }
+        });
+
+        String courseSeeking = getStudentCourse(ptr);
+
+        for(int i=0; i<allUsers.size(); i++){
+            if(allUsers.get(i).getTutor() == 1 && allUsers.get(i).getT_courses().equals(courseSeeking)){
+                System.out.println("Enter randy into list");
+                releventUsers.add(allUsers.get(i));
+            }
+        }
+//        User temp1 = new User();
+//        System.out.println("NAME 1:" + temp1.getName());
+//        temp1.setName("Hermoine Granger");
+//        System.out.println("NAME 1:" + temp1.getName());
+//        temp1.setLocation("Bain");
+//        temp1.setRate(14.50);
+//
+//        User temp2 = new User();
+//        temp2.setName("Ron Weasly");
+//        temp2.setLocation("ILC 2nd Floor Plaza");
+//        temp2.setRate(11.75);
+//
+//
+//        releventUsers = new ArrayList<>();
+//        releventUsers.add(temp1);
+//        releventUsers.add(temp2);
+
         //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         //setSupportActionBar(toolbar);
 
@@ -40,8 +83,10 @@ public class TutorListActivity extends AppCompatActivity {
 
         listItems = new ArrayList<>();
 
-        for(int i= 0; i<10; i++){
-            tutorListItem aTutor = new tutorListItem("Tutor Name" + (i+1), "Location", "$ " + (i), true);
+        System.out.println("USER 1 NAME:" + releventUsers.get(0).getName());
+        //HARDCODED VERSION USING C++ CLASS
+        for(int i = 0; i < releventUsers.size(); i++){
+            tutorListItem aTutor = new tutorListItem(releventUsers.get(i).getName(), releventUsers.get(i).getLocation(), "$" + releventUsers.get(i).getRate(), true);
             listItems.add(aTutor);
         }
 
@@ -54,7 +99,7 @@ public class TutorListActivity extends AppCompatActivity {
                 layoutInflater =(LayoutInflater) getApplicationContext().getSystemService(LAYOUT_INFLATER_SERVICE);
                 ViewGroup container = (ViewGroup) layoutInflater.inflate(R.layout.support_tutor_search_popup, null);
 
-                popupWindow = new PopupWindow(container, 800, 850, true);
+                popupWindow = new PopupWindow(container, 800, 800, true);
                 popupWindow.showAtLocation(recyclerView, Gravity.CENTER, 0, 0);
 
 
